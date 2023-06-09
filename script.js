4adc043b68b848652a061f8f7dbd9521917b06c6
Urii999 <ublaya@hotmail.com>
2023-06-09 16:25:00 +0200

    .

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
-document.getElementById('shortenButton').addEventListener('click', function() {
-  var urlInput = document.getElementById('urlInput');
-  var shortenedUrl = document.getElementById('shortenedUrl');
+function recortarEnlace() {
+  var input = document.getElementById("urlInput");
+  var url = input.value;
 
-  // Verifica si se ingresó una URL válida
-  if (isValidUrl(urlInput.value)) {
-    // Realiza la operación de recorte de enlace aquí
-    var shortened = shortenUrl(urlInput.value);
-    shortenedUrl.textContent = 'Enlace recortado: ' + shortened;
-  } else {
-    shortenedUrl.textContent = 'Ingrese una URL válida.';
-  }
+  // Realiza la solicitud HTTP a un servicio de recorte de enlaces
+  // Aquí se utiliza el servicio "Bit.ly", pero puedes usar otros servicios también
+  fetch("https://api-ssl.bitly.com/v4/shorten", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer 7ff92ff133c126ff466be7f3e0ab963516"
+    },
+    body: JSON.stringify({
+      long_url: url
+    })
+  })
+    .then(response => response.json())
+    .then(data => {
+      if (data && data.link) {
+        var shortenedUrl = data.link;
+        var resultElement = document.getElementById("shortenedUrl");
+        resultElement.textContent = "Enlace recortado: " + shortenedUrl;
 
-  // Limpia el campo de entrada
-  urlInput.value = '';
-});
-
-function isValidUrl(url) {
-  // Verifica si la URL tiene un formato válido (aquí puedes agregar tu lógica de validación)
-  // En este ejemplo, simplemente verifica que no esté vacía
-  return url.trim() !== '';
-}
-
-function shortenUrl(url) {
-  // Implementa la lógica para recortar el enlace aquí (por ejemplo, utilizando una API de recorte de enlaces)
-  // En este ejemplo, simplemente devuelve la URL original
-  return url;
+        // Borrar la información en el campo de entrada
+        input.value = "";
+      } else {
+        console.log("No se pudo recortar el enlace.");
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
 }
